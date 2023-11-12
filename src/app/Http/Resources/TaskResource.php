b6ae0ecb9457204e34a4c9743cdfb1072f9eb635
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-12 12:45:46 +0900

    add:resources追加でjsonで返す

---
+++ src/app/Http/Resources/TaskResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+//  APIでモデルデータをJSONデータとして返す場合、キー名をキャメル<=>スネークの変換や出力したくないデータを除外、データのフォーマットを中間で挟むケースがあります。 これらの処理を簡単に提供できる機能
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
