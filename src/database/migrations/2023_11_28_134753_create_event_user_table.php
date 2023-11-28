88f32a50eddb73197683ac54fd319dae191f5123
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-28 23:06:26 +0900

    add:user event Model定義

--- src/database/migrations/2023_11_28_134753_create_event_user_table.php
+++ src/database/migrations/2023_11_28_134753_create_event_user_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('event_user', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('event_id');
+            $table->string('status_id')->default('0')->comment('0:未回答,1:参加,2:不参加,3:未定');
+            $table->string('comment')->comment('備考');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->timestamps();
         });
     }
